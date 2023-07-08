9ac54c3f885cefd54ea72c7e9a4243b84cbb00aa
AnettVera <20223tn034@utez.edu.mx>
2023-07-08 00:47:38 -0600

    Completo diseño para agregar usuarios

--- js/agregar.js
+++ js/agregar.js
@@ -1,12 +1,15 @@
-var btnAbrir=document.getElementById('btn-abrir'),
-	overlay=document.getElementById('overlay'),
-	pop=document.getElementById('pop-up');
-	btnCerrar=document.getElementById('btn-cerrar');
+var btnAbrir = document.getElementById('btn-abrir');
+var overlay = document.getElementById('overlay');
+var pop = document.getElementById('pop-up');
+var btnCerrar = document.getElementById('btn-cerrar');
+var menuCheckbox = document.getElementById('btn-menu');
 
-
-btnAbrir.addEventListener('click',function () {
-	overlay.classList.add('active');
+btnAbrir.addEventListener('click', function () {
+  overlay.classList.add('active');
+  // Cerrar el menú lateral al abrir el overlay
+  menuCheckbox.checked = false;
 });
-btnCerrar.addEventListener('click',function(){
-	overlay.classList.remove('active');
+
+btnCerrar.addEventListener('click', function () {
+  overlay.classList.remove('active');
 });
